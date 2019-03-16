7639e02fd070c0bda15d1d4946784f6b1d9e11b1
Franklin Farahani <dduction@gmail.com>
2019-03-15 19:15:30 -0700

    change bio from js to tsx

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -9,8 +9,6 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
 
-import { rhythm } from "../utils/typography"
-
 function Bio() {
   return (
     <StaticQuery
@@ -18,17 +16,12 @@ function Bio() {
       render={data => {
         const { author, social } = data.site.siteMetadata
         return (
-          <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
-            }}
-          >
+          <div>
             <Image
               fixed={data.avatar.childImageSharp.fixed}
               alt={author}
               style={{
-                marginRight: rhythm(1 / 2),
+                marginRight: 50,
                 marginBottom: 0,
                 minWidth: 50,
                 borderRadius: `100%`,
