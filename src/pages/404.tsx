6f7e9bf3761e48a77fe333bea045e81034d92841
Franklin Farahani <dduction@gmail.com>
2019-03-15 19:16:19 -0700

    change 404 from js to tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,13 +4,23 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
+interface Props {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+      }
+    }
+  }
+}
+
+class NotFoundPage extends React.Component<Props> {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
