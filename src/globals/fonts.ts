6c81e49779954d09e7083ed68b4cdff3903e10f9
Franklin Farahani <dduction@gmail.com>
2019-03-24 15:53:32 -0700

    add font import exports

---
+++ src/globals/fonts.ts
@@ -0,0 +1,26 @@
+// TT Commons
+import TTCommonsRegularWOFF from '../assets/fonts/ttcommons/ttcommons-regular.woff';
+import TTCommonsRegularWOFF2 from '../assets/fonts/ttcommons/ttcommons-regular.woff2';
+import TTCommonsMediumWOFF from '../assets/fonts/ttcommons/ttcommons-medium.woff';
+import TTCommonsMediumWOFF2 from '../assets/fonts/ttcommons/ttcommons-medium.woff2';
+import TTCommonsDemiboldWOFF from '../assets/fonts/ttcommons/ttcommons-demibold.woff';
+import TTCommonsDemiboldWOFF2 from '../assets/fonts/ttcommons/ttcommons-demibold.woff2';
+import TTCommonsBoldWOFF from '../assets/fonts/ttcommons/ttcommons-bold.woff';
+import TTCommonsBoldWOFF2 from '../assets/fonts/ttcommons/ttcommons-bold.woff2';
+
+import SFMonoRegularWOFF from '../assets/fonts/sfmono/sfmono-regular.woff';
+import SFMonoRegularWOFF2 from '../assets/fonts/sfmono/sfmono-regular.woff2';
+
+
+export {
+  TTCommonsRegularWOFF,
+  TTCommonsRegularWOFF2,
+  TTCommonsMediumWOFF,
+  TTCommonsMediumWOFF2,
+  TTCommonsDemiboldWOFF,
+  TTCommonsDemiboldWOFF2,
+  TTCommonsBoldWOFF,
+  TTCommonsBoldWOFF2,
+  SFMonoRegularWOFF,
+  SFMonoRegularWOFF2,
+};
