4071ff9afdbbaa6347bfa1f4eb8ac67997a5f4bd
Franklin Farahani <dduction@gmail.com>
2019-03-24 17:26:50 -0700

    convert blogpost to typescript

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,37 +4,49 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
 
-class BlogPostTemplate extends React.Component {
+interface Props {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        author: string
+      }
+    }
+    markdownRemark: {
+      id: number
+      excerpt: string
+      html: any
+      frontmatter: {
+        title: string
+        date: string        
+        description: string
+      }
+    }
+  }
+  pageContext: any
+}
+
+class BlogPostTemplate extends React.Component<Props> {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
+    console.log(typeof this.props.pageContext);
+    
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout title={siteTitle}>
         <SEO
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
         <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
+        <p>
           {post.frontmatter.date}
         </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
+        <hr/>
         <Bio />
 
         <ul
