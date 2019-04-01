dab9aeb431e1471256bca73531339c45a60aef02
Franklin Farahani <dduction@gmail.com>
2019-03-31 20:41:40 -0700

    add styling and basic nav menu to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,62 @@ import styled from '@emotion/styled'
 import { Link } from 'gatsby'
 import Headroom from 'react-headroom'
 
+import { config } from '../globals'
 import { Theme } from '../globals/theme'
 import mixins from '../utils/mixins'
 
+import Logo from './Logo'
+
+const {fontSizes} = config
+
 const HeaderContainer = styled.header``
 
+const HeadroomContainer = styled(Headroom)`
+  .headroom {
+    ${mixins.flexBetween}
+    flex-direction: row;
+    padding: 40px 60px;
+    background: ${props => props.theme.bg};
+    z-index: 1000 !important;
+    transition: padding ${config.transition};
+    &.headroom--pinned {
+      padding: 20px 60px;
+      box-shadow: ${config.shadows.medium};
+    }
+  }
+`
+
+const LogoLink = styled(Link)`
+  flex: 1;  
+`
+
+const NavContainer = styled.nav`
+  ${mixins.flexBetween}
+
+`
+
+const NavLink = styled(Link)`
+  position: relative;
+  margin: 0 20px;
+  padding: 0 5px;
+  color: ${props => props.theme.text};
+  font-size: ${fontSizes.nav}px;
+  font-weight: 600;
+  text-decoration: none;
+  &.active {
+    margin-bottom: -4px;
+    border-bottom: 4px solid ${props => props.theme.primary};
+  }
+  &::after {
+    border-bottom: none;
+  }
+`
+
 type HeaderProps = {
-  readonly title?: string
   themeSelect: (theme: Theme) => void
 }
 
-const Header = ({ title, themeSelect }: HeaderProps) => {
+const Header = ({ themeSelect }: HeaderProps) => {
   const [theme, setTheme] = useState(false)
   const themeMap = () => {
     setTheme(!theme)
@@ -21,11 +66,23 @@ const Header = ({ title, themeSelect }: HeaderProps) => {
   }
   return (
     <HeaderContainer>
-      <Headroom>
-        <Link to="/">{title}</Link>
-        <h1>LOGO</h1>
+      <HeadroomContainer>
+        <LogoLink to="/">
+          <Logo />
+        </LogoLink>
+        <NavContainer>
+          <NavLink to="/#work" activeClassName="active" partiallyActive={true}>
+            Work
+          </NavLink>
+          <NavLink to="/blog" activeClassName="active" partiallyActive={true}>
+            Blog
+          </NavLink>
+          <NavLink to="#contact" activeClassName="active" partiallyActive={true}>
+            Contact
+          </NavLink>
+        </NavContainer>
         <input type="checkbox" onChange={() => themeMap()} />
-      </Headroom>
+      </HeadroomContainer>
     </HeaderContainer>
   )
 }
