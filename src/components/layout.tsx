5db768b39f5a2f3cbdd325035ca6ba92aee946db
Franklin Farahani <dduction@gmail.com>
2019-03-31 20:40:59 -0700

    add styling to main comp called Content

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,42 @@
 import React, { Fragment, useState } from 'react'
 import { Global, css } from '@emotion/core'
+import styled from '@emotion/styled'
 import { ThemeProvider } from 'emotion-theming'
 import { Link } from 'gatsby'
 
-import { reset, fonts, themes } from '../globals'
+import { reset, fonts, config, themes } from '../globals'
 import { Theme } from '../globals/theme'
 
 import Header from './Header'
+
+const Content = styled.main`
+  color: ${props=>props.theme.text};
+  background-color: ${props=>props.theme.bg};
+  position: relative;
+  min-height: calc(100vh - 130px);
+  z-index:1;
+  a {
+    margin-bottom: -2px;
+    position: relative;
+    &::after {
+      content: " ";
+      display: block;
+      position: absolute;
+      right:0;
+      left:0;
+      bottom: -2px;
+      border-bottom: 2px solid ${props=>props.theme.primary};
+      transition: ${config.transition};
+      z-index:-1;
+    }
+    &:focus::after,
+    &:hover::after {
+      position: absolute;
+      bottom: 1;
+      border-bottom: 4px solid ${props=>props.theme.primary};
+    }
+  }
+`
 
 type LayoutProps =  {
   title?: string
@@ -20,8 +50,6 @@ const Layout = ({title, children}: LayoutProps) => {
     newTheme === Theme.Light ? setTheme(themes.light) : setTheme(themes.dark)
   }
 
-  
-
   return (    
     <Fragment>
       <Global 
@@ -32,7 +60,7 @@ const Layout = ({title, children}: LayoutProps) => {
       />
       <ThemeProvider theme = {theme}>
         <Header themeSelect = {changeTheme}/>
-        <main style={{background:theme.bg, color: theme.text}}>{children}</main>
+        <Content>{children}</Content>
         <footer>
           © {new Date().getFullYear()}, Built with
           {' '}
