ecf05260327876281ca2c4bc2456d5ca91b6cf83
Franklin Farahani <dduction@gmail.com>
2019-04-02 17:05:07 -0700

    slightly update home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
-
 import { Link } from 'gatsby'
+// import AnchorLink from 'react-anchor-link-smooth-scroll'
+import AnchorLink from '../components/AnchorLink'
+import styled from '@emotion/styled'
 
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
 
-class IndexPage extends React.Component {
-  render() {
-    return(
-      <Layout>
-        <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-          <h1>Franklin.codes</h1>
-          <p>Hi there!</p>
-        <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }} />
-        <Link to="/blog/">Go to Blog</Link>
-      </Layout>
-    )
-  }
-}
+const HeroContainer = styled.section`
+  max-width: 1000px;
+`
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
+    <HeroContainer>
+      <h1 style={{fontSize: 700}}>Hey, I'm Franklin.</h1>
+      <h1>I build digital products.</h1>
+      <h2>I help brands connect with their customers through good design, engaging user experience, and clean code.</h2>
+      <AnchorLink href="#work">
+        <span>Learn More</span>
+        <span>arrow</span>
+      </AnchorLink>
+    </HeroContainer>
+  </Layout>
+)
+  
+
 
 export default IndexPage
