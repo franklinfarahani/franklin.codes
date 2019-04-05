19878d572d4625a95f9d27d6bd0c5111f253ae48
Franklin Farahani <dduction@gmail.com>
2019-04-04 19:38:10 -0700

    create socials float

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import AnchorLink from '../components/AnchorLink'
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
+import mixins from '../utils/mixins'
+
+import {Twitter, Github, LinkedinIn} from 'emotion-icons/fa-brands'
+
 
 const HeroContainer = styled.section`
-  padding: 10vh 0;
+  min-height: calc(100vh - 113px);
+  position: relative;
+  padding-top: calc(17vh + 30 * ((100vw - 320px) / 680));
 
   h1 {
-    font-size: 8.5vw;
+    font-size: 7.5vw;
     line-height: 1;
   }
 
   h2 {
-    max-width: 46%;
+    max-width: 39.5ch;
     font-weight: 400;
     line-height: 1.5;
     margin-top: 4vh;
@@ -22,10 +29,53 @@ const HeroContainer = styled.section`
   }
 `
 
+const ContactFloat = styled.div`
+  ${mixins.flexBetween}
+  flex-direction: column;
+  position: absolute;
+  height: 55vh;
+  right: 0;
+  bottom: 50px;
+`
+
+const Socials = styled.ul`
+  ${mixins.flexBetween}
+  flex-direction: column;
+  height: calc(100px + 30 * ((100vw - 320px) / 680));
+`
+
+const IconTwitter = styled(Twitter)`
+  ${mixins.icon}
+`
+
+const IconGithub = styled(Github)`
+  ${mixins.icon}
+`
+
+const IconLinkedin = styled(LinkedinIn)`
+  ${mixins.icon}
+`
+
+const SayHello = styled.div`
+  font-size: 7px;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <HeroContainer>
+      <ContactFloat>
+        <Socials>
+          <li><IconTwitter /></li>
+          <li><IconGithub /></li>
+          <li><IconLinkedin /></li>
+        </Socials>
+        <SayHello>
+          Say Hello
+        </SayHello>
+
+      </ContactFloat>
+
       <h1>Hey, I'm Franklin.</h1>
       <h1>I build digital products.</h1>
       <h2>I help brands connect with their customers through good design, engaging user experience, and clean code.</h2>
