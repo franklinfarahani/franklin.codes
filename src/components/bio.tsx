a4a7a70306de9b83f60b7dee501a4e91a1c3f556
Franklin Farahani <dduction@gmail.com>
2019-05-17 20:57:45 -0700

    add basic structure to Bio

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,6 +1,6 @@
 /**
- * Bio component that queries for data
- * with Gatsby's StaticQuery component
+ * Blog description component that queries for data
+ * with Gatsby's useStaticQuery hook
  *
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
@@ -8,26 +8,49 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import styled from '@emotion/styled'
 
-function Bio() {
+import { config } from '../globals'
+
+const { fontSizes } = config
+
+const BioContainer = styled.div`
+  width: 500px;
+  padding: 100px 5px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+`
+
+const Image = styled(Img)`
+  display: block;
+  border-radius: 50%;
+  margin-right: 20px;
+`
+
+const BioText = styled.p`
+  font-size: ${fontSizes.text[1]}px;
+`
+
+const Bio = () => {
   const data = useStaticQuery(bioQuery)
   const { author, social } = data.site.siteMetadata
   
   return (
-    <div>
-      <Img
+    <BioContainer>
+      <Image
         fixed={data.avatar.childImageSharp.fixed}
         alt={author}
-        imgStyle={{
-          borderRadius: '50%',
-        }}
       />
-      <p>
-        A coding blog by <a href={`https://twitter.com/${social.twitter}`}>
+      <BioText>
+        A coding blog written by{' '} 
+        <a href={`https://twitter.com/${social.twitter}`}>
           @{social.twitter}
-        </a> about web development, and related topics
-      </p>
-    </div>
+        </a>
+        <br />
+        about web development, and related topics
+      </BioText>
+    </BioContainer>
   )
 }
 
@@ -35,7 +58,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50, quality: 100) {
+        fixed(width: 56, height: 56, quality: 100) {
           ...GatsbyImageSharpFixed
         }
       }
