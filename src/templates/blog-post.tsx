72988b40e5c785c5d4518457268249fdbd22587c
Franklin Farahani <dduction@gmail.com>
2019-05-22 12:41:38 -0700

    convert to functional component

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,10 +4,11 @@ import { Link, graphql } from 'gatsby'
 import BlogDescription from '../components/BlogDescription'
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
+import Tag from '../components/Tag'
 
 // TODO: fix 'any' types
 
-interface Props {
+type BlogPostProps = {
   data: {
     site: {
       siteMetadata: {
@@ -30,11 +31,10 @@ interface Props {
   pageContext: any
 }
 
-class BlogPostTemplate extends React.Component<Props> {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+const BlogPostTemplate = ({data, pageContext} : BlogPostProps) => {
+    const post = data.markdownRemark
+    const siteTitle = data.site.siteMetadata.title
+    const { previous, next } = pageContext
 
     return (
       <Layout title={siteTitle}>
@@ -43,7 +43,7 @@ class BlogPostTemplate extends React.Component<Props> {
           description={post.frontmatter.description || post.excerpt}
         />
         <h1>{post.frontmatter.title}</h1>
-        <p>{post.frontmatter.tags[0]}</p>
+        {post.frontmatter.tags.map((tag, index) => <Tag key={index}>{tag}</Tag>)}
         <p>
           {post.frontmatter.date}
         </p>
@@ -77,7 +77,6 @@ class BlogPostTemplate extends React.Component<Props> {
         </ul>
       </Layout>
     )
-  }
 }
 
 export default BlogPostTemplate
