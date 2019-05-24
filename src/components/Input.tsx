2b26d4f99feac56d0da5c410aab9fb17f6c77314
Franklin Farahani <dduction@gmail.com>
2019-05-24 14:14:36 -0700

    create custom Input comp

---
+++ src/components/Input.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { connectSearchBox } from 'react-instantsearch-dom'
+
+import config from '../globals/config'
+const { borderRadius, transition } = config
+
+const SearchInput = styled.input`
+  outline: none;
+  border: none;
+  font-size: 1em;
+  transition: ${transition};
+  border-radius: ${borderRadius.round};
+`
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+`
+
+type InputProps = {
+  refine: (value: string) => void
+  onFocus: () => void
+}
+
+const Input = ({ refine, ...rest }: InputProps) => {
+  
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    refine(event.target.value)
+  }
+
+  return (
+    <Form>
+      <SearchInput
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+        onChange={handleChange}
+        {...rest}
+      />
+      {/* <SearchIcon /> */}
+    </Form>
+  )
+}
+
+export default connectSearchBox(Input)
