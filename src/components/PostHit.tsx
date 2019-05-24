8e14ae0a937142500e02b27e3239b6604358e2a1
Franklin Farahani <dduction@gmail.com>
2019-05-24 14:14:46 -0700

    create custom PostHit comp

---
+++ src/components/PostHit.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Highlight, Snippet } from 'react-instantsearch-dom'
+import { Link } from 'gatsby'
+
+type PostHitProps = {
+  hit: any
+}
+
+const PostHit = (clickHandler: ()=>void) => ({ hit }: PostHitProps) => (
+  <div>
+    <Link to={'/blog' + hit.fields.slug} onClick={clickHandler}>
+      <h3>
+        <Highlight attribute="title" hit={hit} tagName="mark" />
+      </h3>
+    </Link>
+    <div>
+      &nbsp;
+      <Highlight attribute="date" hit={hit} tagName="mark" />
+    </div>
+    <Snippet attribute="excerpt" hit={hit} tagName="mark" />
+  </div>
+)
+
+export default PostHit
