1b2e0b3178e50854d5adaebc4ca5e3d4093a68eb
Franklin Farahani <dduction@gmail.com>
2019-07-11 12:17:01 -0700

    fix build error with git config

---
+++ src/components/Seo.tsx
@@ -0,0 +1,104 @@
+/**
+ * Seo component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from 'react'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+type SeoProps = {
+  description?: string,
+  lang?: string,
+  meta?: Array<{
+    name?: string,
+    property?: string,
+    content: any,
+  }>,
+  /** meta keywords for the page */
+  keywords?: string[],
+  title?: string,
+} & typeof defaultProps
+
+const defaultProps = {
+  keywords: [''],
+  lang: 'en',
+  meta: [],
+}
+
+function Seo({ description, lang, meta, keywords, title }: SeoProps) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `,
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          content: metaDescription,
+          name: 'description',
+        },
+        {
+          content: title,
+          property: 'og:title',
+        },
+        {
+          content: metaDescription,
+          property: 'og:description',
+        },
+        {
+          content: 'website',
+          property: 'og:type',
+        },
+        {
+          content: 'summary',
+          name: 'twitter:card',
+        },
+        {
+          content: site.siteMetadata.author,
+          name: 'twitter:creator',
+        },
+        {
+          content: title,
+          name: 'twitter:title',
+        },
+        {
+          content: metaDescription,
+          name: 'twitter:description',
+        },
+      ]
+        .concat(
+          keywords.length > 0
+            ? {
+                content: keywords.join(', '),
+                name: 'keywords',
+              }
+            : [],
+        )
+        .concat(meta)}
+    />
+  )
+}
+
+Seo.defaultProps = defaultProps
+
+export default Seo
